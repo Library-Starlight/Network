d904f81a7df69324cdf449e3e852fc931833077c
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-04-29 21:50:11 +0800

    feat
    添加网络解决方案

---
+++ src/Pipeline/Pipeline/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pipeline
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 将Socket包装为NetworkStream；
+        /// 将控制台输入流载入NetworkStream。
+        /// </summary>
+        /// <returns></returns>
+        private async Task ConsoleInputToSocket()
+        {
+            var clientSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Connecting to port 8087");
+
+            clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 8087));
+            var stream = new NetworkStream(clientSocket);
+
+            await Console.OpenStandardInput().CopyToAsync(stream);
+        }
+    }
+}
---
+++ src/Pipeline/Pipeline/helps/ConfigureAwaitInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pipeline.helps
+{
+    class ConfigureAwaitInfo
+    {
+        public async Task RunDemo()
+        {
+            Console.WriteLine($"Thread Information: ThreadId: {Thread.CurrentThread.ManagedThreadId}" +
+                $", IsBackground: {Thread.CurrentThread.IsBackground}" +
+                $", IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+            await new LineProcessor().ProcessLineAsync();
+        }
+
+        private class LineProcessor
+        {
+            public Task ProcessLineAsync()
+            {
+                return ProcessLineAsyncInternal();
+            }
+
+            private async Task ProcessLineAsyncInternal()
+            {
+                await Task.Run(() =>
+                {
+                    Console.WriteLine("Hello World!");
+                }).ConfigureAwait(false);
+
+                Console.WriteLine($"Thread Information: ThreadId: {Thread.CurrentThread.ManagedThreadId}" +
+                    $", IsBackground: {Thread.CurrentThread.IsBackground}" +
+                    $", IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+            }
+        }
+    }
+}
