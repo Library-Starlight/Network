a9ee7186eeda481bcf80361cc7f453fbb4501b3d
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-18 23:02:22 +0800

    feature
    构造Socket基类。
    定义高易用性，安全可靠的事件处理框架。

---
+++ src/AbcClient.UI/AbcClient.Internet/AwesomeClient.cs
@@ -0,0 +1,38 @@
+﻿using AbcClient.Internet.Abstract;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace AbcClient.Internet
+{
+    /// <summary>
+    /// 这是一个高性能、可扩展、高可用、高并发的Tcp客户端
+    /// </summary>
+    public class AwesomeClient : AwesomeSocket
+    {
+        #region 构造函数
+
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        /// <param name="endPoint">服务器终结点</param>
+        public AwesomeClient(IPEndPoint endPoint) : base(endPoint) { }
+
+        #endregion
+
+        #region 公共方法
+
+        /// <summary>
+        /// 启动
+        /// </summary>
+        /// <returns></returns>
+        public override async Task StartAsync()
+        {
+            await Socket.ConnectAsync(EndPoint);
+            await HandleDataAsync(Socket);
+        }
+
+        #endregion
+    }
+}
