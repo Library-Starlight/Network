3d3eebba0f5b49746f2f8ec51ba49dd59c4f7168
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-23 23:10:04 +0800

    feature
    socket udp tcp

---
+++ src/Sockets/Sockets/AdvanceSocket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Sockets
+{
+    public class AdvanceSocket
+    {
+        public void What()
+        {
+            Socket socket;
+
+            TcpClient tcp;
+
+            UdpClient udp;
+        }
+    }
+}
