4a0d6c360d0f49fc2b593b9c8c4960df319ab0b0
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-24 21:46:43 +0800

    feature
    socker项目

--- src/Sockets/Sockets/AdvanceSocket.cs
+++ src/Sockets/Sockets/AdvanceSocket.cs
@@ -1,19 +1,45 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 
 namespace Sockets
 {
     public class AdvanceSocket
     {
-        public void What()
+        public static void What()
         {
             Socket socket;
 
             TcpClient tcp;
 
             UdpClient udp;
+
+            IPHostEntry hostInfo = Dns.GetHostEntry("192.168.0.138");
+
+            Console.WriteLine($"主机：{hostInfo.HostName}");
+            for (int i = 0; i < hostInfo.AddressList.Length; i++)
+            {
+                Console.WriteLine($"地址：{hostInfo.AddressList[i]}");
+            }
+
+            // IPAddress不可序列化
+            //JsonConvert.SerializeObject(IPAddress.Loopback);
+            //Console.WriteLine(JsonSerializer.Serialize(IPAddress.Loopback, new JsonSerializerOptions { WriteIndented = true }));
+            //Console.WriteLine(JsonSerializer.Serialize(host, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        public static void Udp()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            What();
         }
     }
 }
