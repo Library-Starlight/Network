1c7449eb53e38d409e598ef998452756c21227d6
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2021-02-08 23:47:27 +0800

    feature：硬件信息

--- src/Sockets/Sockets/Program.cs
+++ src/Sockets/Sockets/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Sockets.Business;
 using Sockets.Demo;
 
-await new TcpApplicationInfrastructure().StartAsync();
+PerfBlocking.HardwareInfo();
+
+// await new TcpApplicationInfrastructure().StartAsync();
 
 // Tcp echo client and server command line tool
 // await new Tcp().RunCommandLineAsync(args);
