e697a3d95b9e95cf95a0f9bd9429d94d174f0438
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2021-02-27 22:30:01 +0800

    feature：性能测试

---
+++ src/Sockets/Sockets/Demo/AnMillionTcp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sockets.Demo
+{
+    public class AnMillionTcp
+    {
+        public async Task StartClientAsync()
+        {
+
+        }
+
+        public async Task StartServerAsync()
+        {
+
+        }
+    }
+}
